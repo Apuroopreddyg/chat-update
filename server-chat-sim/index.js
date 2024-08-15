4808b1aceb607e37a3c06f202aca7916d787491e
Apuroopreddyg <150883132+Apuroopreddyg@users.noreply.github.com>
2024-08-15 22:11:41 +0530

    Update index.js

--- server-chat-sim/index.js
+++ server-chat-sim/index.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL, {
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
@@ -148,6 +149,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log(`Server is running on port 5000`);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
